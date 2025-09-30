cc2775aa50b5d08e927a3ceb680bb424d7311df2
aziz_boujaada <boujaadaaziz2911@gmail.com>
2025-09-30 16:29:37 +0100

    intializing the structure of clien and products

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variables.h"
+
+int main()
+{
+    int menu_choice;
+    do
+    {
+        printf(YELLOW "============================ SYSTEM DE GESTION D'ACHAT CLIEN ==========================\n" RESET);
+
+        printf(YELLOW "________________________________________ MENU ________________________________________\n" RESET);
+
+        printf(BLUE "1-Gestion du profil client\n" RESET);
+        printf(BLUE "2-Gestion du solde virtuel\n" RESET);
+        printf(BLUE "3-Consultation des produit\n" RESET);
+        printf(BLUE "4-Effectuer un achat\n" RESET);
+        printf(BLUE "5Gestion du profil client\n" RESET);
+        printf(BLUE "0-Quitter l'application\n" RESET);
+
+        printf(CYAN "Enter Votre Choix : " RESET);
+        scanf("%d", &menu_choice);
+
+        switch (menu_choice)
+        {
+        case 1:
+            break;
+
+        case 2:
+            break;
+        }
+    } while (menu_choice != 0);
+
+    return 0;
+}
